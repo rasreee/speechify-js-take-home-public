a4e14d38bc59988ab7c8b720c84fd5dd25544206
rasreee <lesleychang@berkeley.edu>
2021-02-14 15:20:02 -0500

    add styled-components + theme

---
+++ src/client/theme.ts
@@ -0,0 +1,20 @@
+const colors = {
+    light: "#F6F8FC",
+    dark: "#1B263B",
+    bluePrimary: "#114BF5",
+    gray: "#6E7E94",
+    lightBluePurple: "#BFCCFA",
+    error: "#DA4167",
+    text: 'black'
+};
+const fonts = {
+    montserrat: "Montserrat",
+    raleway: "Raleway",
+    rubik: "Rubik",
+};
+const theme = {
+    colors: colors,
+    fonts: fonts,
+};
+
+export default theme;
