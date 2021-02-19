ae3d4131574368b33bb4880939517cad55674c96
rasreee <lesleychang@berkeley.edu>
2021-02-19 08:29:37 -0500

    Update data processor tests

--- src/server/tests/services/data-processor.spec.ts
+++ src/server/tests/services/data-processor.spec.ts
@@ -9,7 +9,7 @@ describe("DataProcessor checks", () => {
                 'Can you please check the latest PR? I just updated the API',
             timeSent: 1613731899778
         })
-        const parsed = DataProcessor.parseJSON(data)
+        const parsed = DataProcessor.parseJSON('https://slack.com/webhooks/chat', data)
         expect(parsed).to.equal('from: @anson\nchannel: #chrome-extension\nmessage: Can you please check the latest PR? I just updated the API\ntimeSent: 1613731899778\n')
     })
 
@@ -21,13 +21,13 @@ describe("DataProcessor checks", () => {
                 'Can you please check the latest PR? I just updated the API',
             timeSent: 1613731899778
         })
-        const parsed = DataProcessor.parseJSON(data)
+        const parsed = DataProcessor.parseJSON('https://slack.com/webhooks/chat', data)
         expect(parsed).to.equal('reading message sent by anson in channel chrome extension on Friday, February 19, 2021 5:51 AM. \ncan you please check the latest PR? I just updated the API. \nmessage ended.')
     })
 
     it("should parse TXT naively", () => {
         const data = 'AMZN\t3232.58\tUSD\nFB\t272.14\tUSD\nAAPL\t142.06\tUSD\nNFLX\t523.28\tUSD'
-        const parsed = DataProcessor.parseTXT(data)
+        const parsed = DataProcessor.parseTXT('feeds.stock-ticker', data)
         expect(parsed).to.equal('the price of amazon is $3232.58 . the price of facebook is $272.14 . the price of apple is $142.06 . the price of netflix is $523.28')
     })
 
