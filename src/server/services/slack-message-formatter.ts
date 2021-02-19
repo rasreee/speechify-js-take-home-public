0bb8ec0f80960aabb1b7d1fe594f0c400036bb94
rasreee <lesleychang@berkeley.edu>
2021-02-19 08:27:33 -0500

    Update Slack message formatter

--- src/server/services/slack-message-formatter.ts
+++ src/server/services/slack-message-formatter.ts
@@ -1,15 +1,32 @@
+type Message = {
+    from: string
+    channel: string
+    message: string
+    timeSent: string
+}
+
+const DAYS = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+
+function formatEpoch(epoch: number): string {
+    const date = new Date(epoch)
+    const hours = date.getHours()
+    const hour = Math.max(hours, hours - 12)
+    const amOrPm = hours >= 12 ? 'PM' : "AM"
+    return `${DAYS[date.getDay()]} ${date.getMonth()} ${date.getUTCDate()}th at ${hour}:${date.getMinutes()}${amOrPm}`
+}
+
 const SlackMessageFormatter = {
     format: (data: string) => {
-        let entries = Object.entries(JSON.parse(data))
+        let parsed: Message = JSON.parse(data)
 
-        let result = ''
+        const author: string = parsed.from.slice(1)
+        const channel: string = parsed.channel.slice(1)
+        const message: string = parsed.message
+        const time: string = formatEpoch(parseInt(parsed.timeSent))
 
-        entries.forEach(entry => {
-            result += entry[0]
-            result += ': '
-            result += entry[1]
-            result += '\n'
-        })
+        let result = `reading message from ${author} in channel ${channel} set at ${time} . `
+        result += `message start . ${message} . message end . `
+
 
         return result;
     }
